633691f7db6b9dc7aff3060e503bd2e22f31c22c
Tanishq510 <tanishqjoshi93@gmail.com>
2022-01-08 13:42:11 +0530

    Woked on user listing and search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
-import React from "react";
-import "./style.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './style.css';
+import { Table, Input, Col, Row } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+// import { addUser } from './store/userSlice';
 
 export default function App() {
+  const APIURL = 'https://randomuser.me/api/?results=40';
+  const { Search } = Input;
+  const [users, setUsers] = useState([]);
+  const [tableUsers, setTableUsers] = useState([]);
+  // const user = useSelector((state) => state.user);
+  // const dispatch = useDispatch();
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  /**
+   * function to fetch users and store in redux store
+   */
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(APIURL);
+      if (response.status === 200) {
+        let usersData = response.data.results;
+        let newUsers = usersData.map((user, index) => {
+          user.key = index;
+          return user;
+        });
+        setUsers(newUsers);
+        setTableUsers(newUsers);
+        //dispatch(addUser)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  /**
+   * function to handle search
+   */
+  const handleSearch = (searchKey) => {
+    if (searchKey) {
+      let filteredUser = users.filter((user) => {
+        return user.name.first.toLowerCase().includes(searchKey.trim());
+      });
+      setTableUsers(filteredUser);
+    }
+  };
+  const columns = [
+    {
+      title: 'Profile Image',
+      dataIndex: 'picture',
+      key: 'name',
+      render: (text) => <img src={text.medium} />,
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => (
+        <a>
+          {text.first} {text.middle} {text.last}
+        </a>
+      ),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'City',
+      dataIndex: 'location',
+      key: 'city',
+      render: (text) => <span>{text.city}</span>,
+    },
+    {
+      title: 'State',
+      dataIndex: 'location',
+      key: 'state',
+      render: (text) => <span>{text.state}</span>,
+    },
+  ];
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <Search
+            placeholder="Search User"
+            onSearch={handleSearch}
+            disabled={!users.length}
+          />
+        </Col>
+        <Col>
+          <Table columns={columns} dataSource={tableUsers} />
+        </Col>
+      </Row>
+    </>
   );
 }
---
+++ src/store/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: [],
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      state = [...state, ...action.payload];
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addUser } = userSlice.actions;
+
+export default userSlice.reducer;
